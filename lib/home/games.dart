78a764c26bcb85ddbabf06bd8dbc833affafb4ac
AhmedHakim <ahmed.hakim@jumppace.com>
2023-01-16 02:14:04 +0500

    -----

--- lib/home/games.dart
+++ lib/home/games.dart
@@ -1,7 +1,10 @@
-import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_screenutil/flutter_screenutil.dart';
+import 'package:fyp/Auth/Signup.dart';
+import 'package:fyp/Auth/login.dart';
 import 'package:fyp/home/profile.dart';
+import 'package:fyp/widget.dart/CustomButton.dart';
+import 'package:fyp/widget.dart/authtxtfield.dart';
 import 'package:get/get.dart';
 
 class GameScreeen extends StatefulWidget {
@@ -12,11 +15,55 @@ class GameScreeen extends StatefulWidget {
 }
 
 class _GameScreeenState extends State<GameScreeen> {
-  final games = FirebaseFirestore.instance.collection('categories').doc().collection('cs').snapshots();
-
   @override
   Widget build(BuildContext context) {
     return Scaffold(
+      // drawer: Drawer(
+      //   width: 0.8.sw,
+      //   backgroundColor: Colors.white,
+      //   shape: RoundedRectangleBorder(
+      //     borderRadius: BorderRadius.only(
+      //       topRight: Radius.circular(25.r),
+      //       bottomRight: Radius.circular(25.r),
+      //     ),
+      //   ),
+      //   child: SafeArea(
+      //     child: ListView(
+      //       children: [
+      //         50.verticalSpace,
+      //         ListTile(
+      //           leading: Icon(
+      //             Icons.home,
+      //             color: Colors.black,
+      //           ),
+      //           title: Text("Game"),
+      //         ),
+      //         Divider(
+      //           color: Colors.grey,
+      //           height: 0,
+      //           thickness: 0.5,
+      //           endIndent: 80.w,
+      //           indent: 10.w,
+      //         ),
+      //         ListTile(
+      //           leading: Icon(
+      //             Icons.person,
+      //             color: Colors.black,
+      //           ),
+      //           title: Text("Profile"),
+      //         ),
+      //         Divider(
+      //           color: Colors.grey,
+      //           height: 0,
+      //           thickness: 0.5,
+      //           endIndent: 80.w,
+      //           indent: 10.w,
+      //         ),
+      //       ],
+      //     ),
+      //   ),
+      // ),
+
       appBar: AppBar(
         leading: GestureDetector(
           onTap: () {
@@ -35,136 +82,91 @@ class _GameScreeenState extends State<GameScreeen> {
           ),
         ),
       ),
-      body: Column(
-        children: [
-          SizedBox(
-            height: 20,
-          ),
-          StreamBuilder<QuerySnapshot>(
-            stream: games,
-            builder: (BuildContext context, AsyncSnapshot<QuerySnapshot> snapshot) {
-              if (snapshot.hasError) {
-                return Text('Something went wrong');
-              }
-
-              if (snapshot.connectionState == ConnectionState.waiting) {
-                return Text("Loading");
-              }
-
-              return Expanded(
-                child: ListView(
-                  children: snapshot.data!.docs.map((DocumentSnapshot document) {
-                    Map<String, dynamic> data = document.data()! as Map<String, dynamic>;
-                    return Column(
-                      children: [
-                        Text(data['game_image'].toString()),
-                        SizedBox(
-                          height: 20,
-                        ),
-                        GestureDetector(
-                          onTap: () {
-                            Get.to(() => GameScreeen());
-                          },
-                          child: Container(
-                            height: 200,
-                            width: 300,
-                            decoration: BoxDecoration(
-                              boxShadow: [
-                                BoxShadow(
-                                  color: Colors.grey.withOpacity(0.5),
-                                  spreadRadius: 5,
-                                  blurRadius: 7,
-                                  offset: Offset(0, 3), // changes position of shadow
-                                ),
-                              ],
-                              image: DecorationImage(image: NetworkImage(data['game_image'].toString()), fit: BoxFit.fill),
-                              borderRadius: BorderRadius.circular(10.r),
-                            ),
-                          ),
-                        ),
-                        SizedBox(
-                          height: 20,
-                        ),
-                      ],
-                    );
-                  }).toList(),
+      body: Container(
+        width: double.infinity,
+        child: SingleChildScrollView(
+          child: Padding(
+            padding: EdgeInsets.symmetric(horizontal: 20.w),
+            child: Column(
+              children: [
+                20.verticalSpace,
+                Text(
+                  "Select Game",
+                  style: TextStyle(
+                    color: Colors.black,
+                    fontSize: 18.sp,
+                  ),
                 ),
-              );
-            },
-          )
-        ],
+                20.verticalSpace,
+                Wrap(
+                  runSpacing: 20.h,
+                  children: [
+                    GestureDetector(
+                      onTap: () => Get.to(() => ProfileScreen()),
+                      child: Container(
+                        height: 200,
+                        width: 300,
+                        decoration: BoxDecoration(
+                          boxShadow: [
+                            BoxShadow(
+                              color: Colors.grey.withOpacity(0.5),
+                              spreadRadius: 5,
+                              blurRadius: 7,
+                              offset:
+                                  Offset(0, 3), // changes position of shadow
+                            ),
+                          ],
+                          image: DecorationImage(
+                              image: AssetImage("assets/csgo.jpg"),
+                              fit: BoxFit.fill),
+                          borderRadius: BorderRadius.circular(10.r),
+                        ),
+                      ),
+                    ),
+                    Container(
+                      height: 200,
+                      width: 300,
+                      decoration: BoxDecoration(
+                        boxShadow: [
+                          BoxShadow(
+                            color: Colors.grey.withOpacity(0.5),
+                            spreadRadius: 5,
+                            blurRadius: 7,
+                            offset: Offset(0, 3), // changes position of shadow
+                          ),
+                        ],
+                        image: DecorationImage(
+                            image: AssetImage("assets/dota2.jpg"),
+                            fit: BoxFit.fill),
+                        borderRadius: BorderRadius.circular(10.r),
+                      ),
+                    ),
+                    Container(
+                      height: 200,
+                      width: 300,
+                      decoration: BoxDecoration(
+                        boxShadow: [
+                          BoxShadow(
+                            color: Colors.grey.withOpacity(0.5),
+                            spreadRadius: 5,
+                            blurRadius: 7,
+                            offset: Offset(0, 3), // changes position of shadow
+                          ),
+                        ],
+                        image: DecorationImage(
+                            image: AssetImage("assets/pubg.jpg"),
+                            fit: BoxFit.fill),
+                        borderRadius: BorderRadius.circular(10.r),
+                      ),
+                    ),
+                  ],
+                ),
+                20.verticalSpace
+              ],
+            ),
+          ),
+        ),
       ),
-
-      // Container(
-      //   width: double.infinity,
-      //   child: SingleChildScrollView(
-      //     child: Padding(
-      //       padding: EdgeInsets.symmetric(horizontal: 20.w),
-      //       child: Column(
-      //         children: [
-      //           20.verticalSpace,
-      //           Text(
-      //             "Select Game",
-      //             style: TextStyle(
-      //               color: Colors.black,
-      //               fontSize: 18.sp,
-      //             ),
-      //           ),
-      //           20.verticalSpace,
-      //           StreamBuilder<QuerySnapshot>(
-      //             stream: games,
-      //             builder: (BuildContext context, AsyncSnapshot<QuerySnapshot> snapshot) {
-      //               if (snapshot.hasError) {
-      //                 return Text('Something went wrong');
-      //               }
-
-      //               if (snapshot.connectionState == ConnectionState.waiting) {
-      //                 return Text("Loading");
-      //               }
-
-      //               return Expanded(
-      //                 child: ListView(
-      //                   children: snapshot.data!.docs.map((DocumentSnapshot document) {
-      //                     Map<String, dynamic> data = document.data()! as Map<String, dynamic>;
-      //                     return Column(
-      //                       children: [
-      //                         GestureDetector(
-      //                           onTap: () {
-      //                             Get.to(() => GameScreeen());
-      //                           },
-      //                           child: Container(
-      //                             height: 200,
-      //                             width: 300,
-      //                             decoration: BoxDecoration(
-      //                               boxShadow: [
-      //                                 BoxShadow(
-      //                                   color: Colors.grey.withOpacity(0.5),
-      //                                   spreadRadius: 5,
-      //                                   blurRadius: 7,
-      //                                   offset: Offset(0, 3), // changes position of shadow
-      //                                 ),
-      //                               ],
-      //                               image: DecorationImage(image: NetworkImage(data['game_image'].toString()), fit: BoxFit.fill),
-      //                               borderRadius: BorderRadius.circular(10.r),
-      //                             ),
-      //                           ),
-      //                         ),
-      //                         SizedBox(
-      //                           height: 20,
-      //                         ),
-      //                       ],
-      //                     );
-      //                   }).toList(),
-      //                 ),
-      //               );
-      //             },
-      //           ),
-      //           20.verticalSpace
-      //         ],
-      //       ),
-      //     ),
-      //   ),
-      // ),
     );
   }
 }
